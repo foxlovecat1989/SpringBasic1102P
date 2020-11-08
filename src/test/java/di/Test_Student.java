c27488a57dfba364f6e75c21ed51c3828fad9e4c
Huang Kai Chen <foxlovecat1989@gmail.com>
2020-11-08 16:59:25 +0800

    SpringBasic1102P - config & environment

--- src/test/java/di/Test_Student.java
+++ src/test/java/di/Test_Student.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package di;
+
+import com.mycompany.springbasic1102p.di.Student;
+import org.junit.Test;
 
 /**
  *
  * @author foxlo
  */
 public class Test_Student {
-    
+    @Test 
+    public void test() {
+        Student s1 = new Student();
+        s1.setName("Vincent");
+        s1.setAge(18);
+        System.out.println(s1);
+    }
 }
